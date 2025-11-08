bf9fcea5f8b1df967b369f50a0cb8a267ef90844
mudanimates <mudanimates@gmail.com>
2025-11-08 12:25:24 -0600

    Added files
    efegsegr

---
+++ Plugin.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using BepInEx;
+using GorillaGameModes;
+using GorillaNetworking;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using Utilla;
+using Utilla.Attributes;
+using Utilla.Models;
+
+namespace yo
+{
+	/// <summary>
+	/// This is your mod's main class.
+	/// </summary>
+
+	/* This attribute tells Utilla to look for [ModdedGameJoin] and [ModdedGameLeave] */
+	[ModdedGamemode]
+	[BepInDependency("org.legoandmars.gorillatag.utilla", "1.5.0")]
+	[BepInPlugin(PluginInfo.GUID, PluginInfo.Name, PluginInfo.Version)]
+	public class Plugin : BaseUnityPlugin
+	{
+		bool g = true;
+		bool inRoom;
+
+		void Start()
+		{
+			/* A lot of Gorilla Tag systems will not be set up when start is called /*
+			/* Put code in OnGameInitialized to avoid null references */
+
+			Utilla.Events.GameInitialized += OnGameInitialized;
+		}
+
+		void OnEnable()
+		{
+			/* Set up your mod here */
+			/* Code here runs at the start and whenever your mod is enabled*/
+
+			HarmonyPatches.ApplyHarmonyPatches();
+		}
+
+		void OnDisable()
+		{
+			/* Undo mod setup here */
+			/* This provides support for toggling mods with ComputerInterface, please implement it :) */
+			/* Code here runs whenever your mod is disabled (including if it disabled on startup)*/
+
+			HarmonyPatches.RemoveHarmonyPatches();
+		}
+
+		void OnGameInitialized(object sender, EventArgs e)
+		{
+			/* Code here runs after the game initializes (i.e. GorillaLocomotion.Player.Instance != null) */
+		}
+
+		void Update()
+		{
+			/* Code here runs every frame when the mod is enabled */
+			if (g)
+			{
+				if (Keyboard.current.jKey.isPressed == true)
+				{
+                    WatchableStringSO s = new WatchableStringSO
+                    {
+                        Value = "MODDED_Casual"
+                    };
+                    g = false;
+					GorillaComputer.instance.currentGameMode = s;
+					GorillaComputer.instance.GetJoinTriggerForZone("forest").OnBoxTriggered();
+					
+				}
+			}
+            if (!Keyboard.current.jKey.isPressed == true)
+            {
+				g = true;
+            }
+        }
+
+		/* This attribute tells Utilla to call this method when a modded room is joined */
+		[ModdedGamemodeJoin]
+		public void OnJoin(string gamemode)
+		{
+			/* Activate your mod here */
+			/* This code will run regardless of if the mod is enabled*/
+
+			inRoom = true;
+		}
+
+		/* This attribute tells Utilla to call this method when a modded room is left */
+		[ModdedGamemodeLeave]
+		public void OnLeave(string gamemode)
+		{
+			/* Deactivate your mod here */
+			/* This code will run regardless of if the mod is enabled*/
+
+			inRoom = false;
+		}
+	}
+}
---
+++ PluginInfo.cs
@@ -0,0 +1,12 @@
+﻿namespace yo
+{
+	/// <summary>
+	/// This class is used to provide information about your mod to BepInEx.
+	/// </summary>
+	class PluginInfo
+	{
+		public const string GUID = "com.mudanimates.gorillatag.yo";
+		public const string Name = "yo";
+		public const string Version = "1.0.0";
+	}
+}
